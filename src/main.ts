688ed18494d22588396ac506269c2eae6c47ba37
Osvaldx <romanonahuel24@gmail.com>
2025-10-30 17:09:51 -0300

    [+] se agrego una configuración para que recibir datos no esperados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración para evitar recibir cosas que no queremos por body.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
