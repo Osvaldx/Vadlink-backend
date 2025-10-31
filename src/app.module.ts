e822c6303ffb0c5aaa28a7531b202c3244026292
Osvaldx <romanonahuel24@gmail.com>
2025-10-30 22:32:47 -0300

    [+] se hizo la conexión a mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -10,7 +13,10 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
       envFilePath: ".env"
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI!)
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
+    UsersModule,
+    PostsModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
