bc05a887295e37f94a0a1b0d6b2601c4a4ac9c4a
Osvaldx <romanonahuel24@gmail.com>
2025-11-02 23:57:51 -0300

    [+] se agregaron más campos a la entidad usuario

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -11,14 +11,26 @@ export class User {
     @Prop()
     lastName: string;
 
-    @Prop({ required: true })
-    age: number;
+    @Prop({ required: true})
+    username: string;
+
+    @Prop({enum: ['usuario', 'administrador'], default: 'usuario'})
+    rol: string;
+
+    @Prop()
+    description: string
+
+    @Prop({ required: true, type: Date })
+    dateofbirth: Date;
 
     @Prop({ required: true, unique: true })
     email: string;
 
     @Prop({ required: true })
     password: string;
+
+    @Prop()
+    avatar: string
 
     @Prop({ default: new Date() })
     createDate: Date;
