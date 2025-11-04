c0c8116079e0e4ab58b3b4f8bbe27f9427ddbdb3
Osvaldx <romanonahuel24@gmail.com>
2025-11-04 19:25:01 -0300

    [+] se agrego una ruta para poder subir la imagen a cloudinary y acutalizar la foto de perfil del user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, Post, UseGuards, UseInterceptors, UploadedFile, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtGuard } from 'src/guards/jwt/jwt.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import type { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -14,6 +17,13 @@ export class UsersController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
+  }
+
+  @UseGuards(JwtGuard)
+  @Post('/upload/avatar')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadAvatar(@UploadedFile() file: Express.Multer.File, @Req() req: Request) {
+    return this.usersService.uploadAvatar(file, req);
   }
 
   @Patch(':id')
