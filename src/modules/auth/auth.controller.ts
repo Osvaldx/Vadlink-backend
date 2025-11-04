30cc9c7419fffd9e1fda48da88c31930cab78936
Osvaldx <romanonahuel24@gmail.com>
2025-11-04 19:22:55 -0300

    [+] se agregaron los parametros de response para usarlos en los servicios

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthUserDto } from './dto/auth-user-dto';
+import type { Request, Response } from 'express';
+import { JwtGuard } from 'src/guards/jwt/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,13 +11,19 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post("/register")
-    public signUp(@Body() createUserDto: CreateUserDto) {
-        return this.authService.signUp(createUserDto);
+    public signUp(@Body() createUserDto: CreateUserDto, @Res() response: Response) {
+        return this.authService.signUp(createUserDto, response);
     }
 
     @Post("/login")
-    public signIn(@Body() authUserDto: AuthUserDto) {
-        return this.authService.signIn(authUserDto);
+    public signIn(@Body() authUserDto: AuthUserDto, @Res({ passthrough: true }) response: Response) {
+        return this.authService.signIn(authUserDto, response);
+    }
+
+    @UseGuards(JwtGuard)
+    @Post('/data')
+    public verify(@Req() request: Request) {
+        return this.authService.verify(request);
     }
 
 }
