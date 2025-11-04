502ac3d03d0decc2a4550d4308a70d62312a8b58
Osvaldx <romanonahuel24@gmail.com>
2025-11-04 19:21:55 -0300

    [+] se configuraron los cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ['error', 'warn', 'debug', 'log', 'verbose'] });
+
+  // habilitamos los cors
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
+  // configuramos de cookies
+  app.use(cookieParser());
 
   // Configuración para evitar recibir cosas que no queremos por body.
   app.useGlobalPipes(
@@ -15,4 +27,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
