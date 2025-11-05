8953132dd67c1d6aec5500106898d2814353b226
Osvaldx <romanonahuel24@gmail.com>
2025-11-05 00:16:07 -0300

    [+] cambiamos el register para que use un fileinterceptor

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, Res, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Res, Req, UseGuards, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthUserDto } from './dto/auth-user-dto';
 import type { Request, Response } from 'express';
 import { JwtGuard } from 'src/guards/jwt/jwt.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('auth')
 export class AuthController {
@@ -11,13 +12,20 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post("/register")
-    public signUp(@Body() createUserDto: CreateUserDto, @Res() response: Response) {
-        return this.authService.signUp(createUserDto, response);
+    @UseInterceptors(FileInterceptor('avatar'))
+    public async signUp(@UploadedFile() file: Express.Multer.File, @Body() createUserDto: CreateUserDto, @Res({ passthrough: true }) response: Response) {
+        return this.authService.signUp(createUserDto, response, file);
     }
 
     @Post("/login")
     public signIn(@Body() authUserDto: AuthUserDto, @Res({ passthrough: true }) response: Response) {
         return this.authService.signIn(authUserDto, response);
+    }
+
+    @UseGuards(JwtGuard)
+    @Post('/logout')
+    public signOut(@Res({ passthrough: true }) response: Response) {
+        this.authService.signOut(response);
     }
 
     @UseGuards(JwtGuard)
