42996cd7fe3b8d28380231091a4689eb36869f7e
Osvaldx <romanonahuel24@gmail.com>
2025-11-08 22:11:28 -0300

    [+] registramos el esquema

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Post, PostSchema } from './entities/post.entity';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Post.name, schema: PostSchema}]), CloudinaryModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
