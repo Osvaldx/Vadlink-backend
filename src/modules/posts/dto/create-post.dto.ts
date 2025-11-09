9bb9db709338fb44ba4a37a2d58d1041b4a6d8db
Osvaldx <romanonahuel24@gmail.com>
2025-11-08 22:10:45 -0300

    [+] desarrollamos los parametros que recibe y validamos

--- src/modules/posts/dto/create-post.dto.ts
+++ src/modules/posts/dto/create-post.dto.ts
@@ -1,1 +1,35 @@
-export class CreatePostDto {}
+import { IsNumber, IsOptional, IsString, Length, Min } from "class-validator"
+import type { ObjectId } from "mongoose"
+
+export class CreatePostDto {
+
+    @IsString()
+    @Length(3, 100)
+    title: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(10, 2000)
+    description: string;
+
+    @IsNumber()
+    @Min(0)
+    likes: number;
+
+    @IsNumber()
+    @Min(0)
+    shared: number;
+
+    @IsOptional()
+    user_id: ObjectId;
+
+    @IsOptional()
+    username: string
+
+    @IsOptional()
+    url_img: string;
+
+    @IsOptional()
+    url_img_id: string;
+
+}
