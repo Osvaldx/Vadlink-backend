f25e0e370e180aa895878218fb0d163be2d76f30
Osvaldx <romanonahuel24@gmail.com>
2025-11-08 22:11:48 -0300

    [+] desarrollamos toda la logica de todos lso emtodos de posts, validamos y odo

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,26 +1,157 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { Request } from 'express';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post } from './entities/post.entity';
+import { Model } from 'mongoose';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { Likes, PostDate } from './posts.controller';
+import { ValidateObjectID } from 'src/common/utils/validate-object-id';
+
+type Filters = {
+  username?: string | null,
+  date?: PostDate | null,
+  likes?: Likes | null,
+  limit?: number,
+  offset?: number
+}
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
-  }
 
-  findAll() {
-    return `This action returns all posts`;
-  }
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>, private readonly cloudService: CloudinaryService) { };
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
+  // --------------------------------------------------------------------------------------- //
+  async create(file: Express.Multer.File, createPostDto: CreatePostDto, request: Request) {
+    const payload = request['user'];
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
+    if(!createPostDto.user_id) {
+      createPostDto.user_id = payload.id;
+      createPostDto.username = payload.username;
+    }
+    
+    if(file) {
+      const extension = file.originalname.split('.').pop();
+      if(extension && !['png', 'jpg', 'jpeg', 'webp'].includes(extension)) {
+        throw new BadRequestException('Formato de imagen invalido');
+      }
+      
+      const uploadFile = await this.cloudService.uploadPost(file);
+      createPostDto.url_img = uploadFile.secure_url;
+      createPostDto.url_img_id = uploadFile.public_id;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+    const createdPost = new this.postModel(createPostDto);
+    const result = createdPost.save();
+
+    return result;
   }
+  // --------------------------------------------------------------------------------------- //
+    
+  // --------------------------------------------------------------------------------------- //
+  async findAll(filters: Filters, request: Request) {
+    const query = this.postModel.find()
+    const payload = request['user'];
+
+    if(payload.rol != 'admin') {
+      query.where('isDeleted').equals(false);
+    }
+
+    if(filters.username) {
+      query.where('username').equals(filters.username);
+    }
+
+    if(filters.date) {
+      query.sort({ created_at: (filters.date === 'asc') ? 'asc' : 'desc' });
+    }
+
+    if(filters.likes) {
+      query.sort({ likes: (filters.likes === 'asc') ? 'asc' : 'desc' });
+    }
+
+    if(filters.limit) {
+      query.limit(filters.limit);
+    }
+
+    if(filters.offset) {
+      query.skip(filters.offset);
+    }
+
+    const posts = await query.exec();
+
+    return posts;
+  }
+  // --------------------------------------------------------------------------------------- //
+    
+  // --------------------------------------------------------------------------------------- //
+  async remove(id: string, request: Request) {
+    ValidateObjectID(id);
+
+    const payload = request['user'];
+    
+    if(payload.rol != 'admin') {
+      const post = await this.postModel.findById(id);
+      if(post?.user_id != payload.id) {
+        throw new BadRequestException('No puedes eliminar esta publicación');
+      }
+    }
+
+    const result = await this.postModel.updateOne({ _id: id }, { isDeleted: true });
+
+    if(result.modifiedCount > 0) {
+      return { message: 'Publicación eliminada con exito!' };
+    }
+
+    return { message: 'No se encontro una publicación con ese ID' };
+  }
+  // --------------------------------------------------------------------------------------- //
+  
+  // --------------------------------------------------------------------------------------- //
+  async like(id: string, request: Request) {
+    ValidateObjectID(id);
+
+    const post = await this.postModel.findById(id);
+    
+    if(!post) {
+      throw new NotFoundException('No se encontro una publicación con ese ID');
+    }
+
+    const payload = request['user'];
+    
+    if(post.likedBy.includes(payload.id)) {
+      throw new BadRequestException('Ya has likeado esta publicación');
+    }
+
+    post.likedBy.push(payload.id);
+    const result = await post.save();
+    
+    return { message: 'Se registro el like a la publicación', post_id: result._id };
+  }
+  // --------------------------------------------------------------------------------------- //
+    
+  // --------------------------------------------------------------------------------------- //
+  async removeLike(id: string, request: Request) {
+    ValidateObjectID(id);
+
+    const post = await this.postModel.findById(id);
+
+    if(!post) {
+      throw new NotFoundException('No se encontro una publicación con ese ID');
+    }
+
+    const payload = request['user'];
+
+    if(!post.likedBy.includes(payload.id)) {
+      throw new BadRequestException('La publicación no tiene tu like');
+    }
+
+    post.likedBy = post.likedBy.filter(p => p._id != payload.id);
+    const result = await post.save();
+
+    return { message: 'Se saco el like de la publicidad', post_id: result._id };
+  }
+  // --------------------------------------------------------------------------------------- //
 }
