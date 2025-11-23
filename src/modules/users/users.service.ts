587ed0f6a5a4756eecf72160b8d184e99345bbe9
Osvaldx <romanonahuel24@gmail.com>
2025-11-23 19:34:47 -0300

    [+] se agregaron metodos y se trajo el crear usuario para este modulo, pq tiene q estar aca

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable, UseGuards } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
@@ -7,6 +7,10 @@ import { Model } from 'mongoose';
 import { Request } from 'express';
 import { CloudinaryService } from '../../cloudinary/cloudinary.service';
 import { ValidateObjectID } from '../../common/utils/validate-object-id';
+import { IsAdminGuard } from 'src/guards/is-admin/is-admin.guard';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
+import * as bcrypt from 'bcrypt';
 
 export interface userJwtData {
   id: string,
@@ -22,12 +26,28 @@ export class UsersService {
 
   constructor(@InjectModel(User.name) private userModel: Model<User>, private readonly cloudService: CloudinaryService) {}
 
+  @UseGuards(IsAdminGuard)
   async findAll() {
     const result = await this.userModel.find();
     return result;
   }
 
-  async findOne(id: string) {
+  async findOneEmailOrUsername(email?: string, username?: string) {
+    const or: any[] = [];
+
+    if(email) {
+      or.push({ email: email.toLowerCase() });
+    }
+
+    if(username) {
+      or.push({ username });
+    }
+
+    return await this.userModel.findOne({ $or: or });
+  }
+
+  // ---------------------------------- //
+  async findOneById(id: string) {
     const result = await this.userModel.findOne({ _id: id });
     return result;
   }
@@ -36,12 +56,56 @@ export class UsersService {
     const result = await this.userModel.updateOne({ _id: id }, updateUserDto);
     return result;
   }
+  // ---------------------------------- //
+
+  async disableOrEnableUser(userId: string, value: boolean) {
+    ValidateObjectID(userId);
+
+    const userDB = await this.findOneById(userId);
+    if(!userDB) throw new HttpException('No se encontro ese usuario', HttpStatus.NOT_FOUND);
+
+    userDB.isDisabled = value;
+    const result = await userDB.save();
+
+    if(result) {
+      throw new HttpException('Estado de usuario actualizado', HttpStatus.OK);
+    }
+
+    throw new HttpException('No se pudo actualizar el estado del usuario', HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 
   async remove(id: string) {
     ValidateObjectID(id);
     const result = await this.userModel.deleteOne({ _id: id });
 
     return { message: (result.deletedCount >= 1) ? 'Se elimino el usuario' : 'No se elimino ningun usuario' };
+  }
+
+  async create(createUserDto: CreateUserDto, file?: Express.Multer.File) {
+    const userEmail = await this.findOneEmailOrUsername(createUserDto.email.toLowerCase());
+    // const userEmail = await this.userModel.findOne({ email: createUserDto.email.toLowerCase() });
+    if (userEmail) throw new HttpException("Ese Email ya esta registrado", HttpStatus.CONFLICT);
+  
+    let uploadResult: UploadApiResponse | UploadApiErrorResponse | null = null;
+    if (file) {
+        uploadResult = await this.cloudService.uploadAvatar(file);
+    }
+  
+    const newUser = new this.userModel({
+      ...createUserDto,
+      email: createUserDto.email.toLowerCase(),
+      avatar: uploadResult?.secure_url || process.env.AVATAR_DEFAULT,
+      avatar_id: uploadResult?.public_id || process.env.AVATAR_DEFAULT_ID,
+      banner: process.env.BANNER_DEFAULT,
+      banner_id: process.env.BANNER_DEFAULT_ID,
+    });
+  
+    const hashPass = await bcrypt.hash(newUser.password, 10);
+    newUser.set("password", hashPass);
+  
+    const createdUser = await newUser.save();
+
+    return createdUser;
   }
 
   async uploadAvatar(file: Express.Multer.File, req: Request) {
@@ -59,7 +123,7 @@ export class UsersService {
       throw new HttpException('No se pudo obtener el usuario autenticado', HttpStatus.UNAUTHORIZED);
     }
 
-    const userDB = await this.findOne(user.id);
+    const userDB = await this.findOneById(user.id);
 
     if (!userDB) {
       throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
